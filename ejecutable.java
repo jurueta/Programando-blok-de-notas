bc90e2538f52c4f9d9aa01639f5bfff4599422eb
Jurueta <juruetacantillo@gmail.com>
2018-08-09 13:01:23 -0500

    Se agrego una nueva variable que se llama correo

--- ejecutable.java
+++ ejecutable.java
@@ -7,23 +7,24 @@ private String nombre;
 private String apellido;
 private String cedula;
 private int edad;
+private String correo;
 
 public void PruebaObj(){
 
 }
 
-public void PruebaObj(String nombre, String apellido,String cedula, int edad){
+public void PruebaObj(String nombre, String apellido,String cedula, int edad, String correo){
 this.nombre = nombre;
 this.apellido = apellido;
 this.cedula = cedula
 this.edad = edad;
-
+this.correo = correo;
 }
 
 
 @Override
 public String toString (){
-return "Nombre= "+nombre+" Apellido= "+apellido+"Cedula= "+cedula+" Edad= "+edad;
+return "Nombre= " + nombre + " Apellido= " + apellido + "Cedula= " + cedula + " Edad= " + edad + " Correo= " + Correo;
 }
 }
 
@@ -31,15 +32,15 @@ public class ejecutable{
 
 public static void main(String[] args){
 
-String nom, ape, cedula;
+String nom, ape, cedula, correo;
 int edad;
 PruebaObj persona = new PruebaObj();
 nom = JOptionPane.showInputDialog(null, "Ingrese su nombre");
 ape = JOptionPane.showInputDialog(null, "Ingrese su apellido");
 cedula = JOptionPane.ShowInputDialog(null,"Ingrese su cedula");
 edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese su edad"));
-
-persona.PruebaObj(nom, ape, cedula, edad);
+correo = JOptionPane.ShowInputDialog(null, "Ingrese su Correo");
+persona.PruebaObj(nom, ape, cedula, edad, Correo);
 System.out.println(persona);
 }
 
